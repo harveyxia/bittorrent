f3926b4833e047308d213834b76b9363ad2cadc9
Jason Kim <jasonkim.0815@gmail.com>
2016-05-02 16:25:07 -0400

    create Client function signatures

--- core/Client.java
+++ core/Client.java
@@ -1,7 +1,59 @@
 package core;
 
+import utils.DataFile;
+
+import java.net.Inet4Address;
+import java.util.HashMap;
+
 /**
  * Bittorrent client.
  */
 public class Client {
+
+    private HashMap<Inet4Address, ConnectionState> peers;
+
+    public Client() {
+        this.peers = new HashMap<>();
+    }
+
+    public static void main(String[] argv) {
+    }
+
+    public Inet4Address[] getPeers() {
+        // TODO: get peer list from tracker
+        return new Inet4Address[0];
+    }
+
+    public void connectPeer(Inet4Address peer) {
+        // TODO: connect with peer
+    }
+
+    public void sendHave(Inet4Address peer) {
+    }
+
+    public void sendChoke(Inet4Address peer) {
+    }
+
+    public void sendUnchoke(Inet4Address peer) {
+    }
+
+    public void sendInterested(Inet4Address peer) {
+    }
+
+    public void sendUninterested(Inet4Address peer) {
+    }
+
+    // TODO: implementing keep-alive messages?
+    public void sendKeepAlive(Inet4Address peer) {
+    }
+
+    public void sendRequest(Inet4Address peer, int index, int begin, int length) {
+    }
+
+    public void sendPiece(Inet4Address peer, DataFile file, int index, int begin, int length) {
+    }
+
+    // TODO: do we need this if not implementing end-game behavior?
+    public void sendCancel(Inet4Address peer) {
+    }
 }
