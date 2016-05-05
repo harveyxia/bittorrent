0a784b72dcfce002e2e9bc8ac52a786ad118bca8
Charles Jin <cjin32@gmail.com>
2016-05-05 03:29:15 -0400

    made testing a look a bit nicer

--- tests/TestRunner.java
+++ tests/TestRunner.java
@@ -7,10 +7,22 @@ import org.junit.runner.notification.Failure;
 public class TestRunner {
     public static void main(String[] args) {
         Class[] classes = {MetafileUtilsTest.class, MessageParserTest.class, MessageBuilderTest.class};
-        Result result = JUnitCore.runClasses(MetafileUtilsTest.class);
+
+        System.out.println("\n=================="  );
+        System.out.println(  "    begin test    "  );
+        System.out.println(  "==================\n");
+
+        Result result = JUnitCore.runClasses(classes);
         for (Failure failure : result.getFailures()) {
             System.out.println(failure.toString());
         }
-        System.out.println(result.wasSuccessful());
+
+        System.out.println("\n==================");
+        if (result.wasSuccessful()){
+          System.out.println(" tests successful ");
+        } else {
+          System.out.println(" tests failed.... ");
+        }
+        System.out.println("==================\n");
     }
 }
