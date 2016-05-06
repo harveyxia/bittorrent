101041e5fc902d1fd3c2d1b256618b3c7e28f133
Jason Kim <jasonkim.0815@gmail.com>
2016-05-06 15:26:19 -0400

    Add error log message to Uploader.receiveRequest().

--- core/Uploader.java
+++ core/Uploader.java
@@ -33,12 +33,14 @@ public class Uploader {
     }
 
     public void receiveRequest(Connection connection, DataFile dataFile, int pieceIndex) {
+        if (!connection.canUploadTo()) {
+            logger.log(" ERROR: cannot upload to " + connection.getSocket().getInetAddress());
+            return;
+        }
+        byte[] pieceMessage = MessageBuilder.buildPiece(pieceIndex, 0, dataFile.readPiece(pieceIndex));
+        MessageSender.sendMessage(connection.getSocket(), pieceMessage);
         logger.log(String.format(" receive REQUEST for pieceIndex:%d from " +
                 connection.getSocket().getInetAddress(), pieceIndex));
-        if (connection.canUploadTo()) {
-            byte[] pieceMessage = MessageBuilder.buildPiece(pieceIndex, 0, dataFile.readPiece(pieceIndex));
-            MessageSender.sendMessage(connection.getSocket(), pieceMessage);
-        }
     }
 
 }
