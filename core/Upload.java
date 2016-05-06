5a0d5fce481300fc3a16215c1a62b761acef151b
Jason Kim <jasonkim.0815@gmail.com>
2016-05-05 22:17:31 -0400

    make Upload function signatures

--- core/Upload.java
+++ core/Upload.java
@@ -1,7 +1,17 @@
 package core;
 
 /**
- * Harvey Xia.
+ * Uploader protocol functions.
  */
 public class Upload {
+
+    public static void receivedInterested(Connection connection, State state) {
+    }
+
+    public static void receivedUninterested(Connection connection, State state) {
+    }
+
+    public static void receivedRequest(Connection connection, State state) {
+    }
+
 }
