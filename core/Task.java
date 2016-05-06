a87c1c10b7f273ae04e962010384f26ce84e18af
Harvey Xia <harvey@harveyxia.com>
2016-05-05 22:46:02 -0400

    Start Task and Download.

--- core/Task.java
+++ core/Task.java
@@ -1,22 +1,48 @@
 package core;
 
 import message.Message;
+import utils.DataFile;
 
 /**
- * Created by marvin on 5/5/16.
+ * Delegates events based on received message type.
  */
 public class Task implements Runnable {
 
     private Connection connection;
     private Message message;
+    private DataFile datafile;
 
-    public Task(Connection connection, Message message) {
+    public Task(Connection connection, Message message, DataFile datafile) {
         this.connection = connection;
         this.message = message;
+        this.datafile = datafile;
     }
 
     @Override
     public void run() {
-        // TODO actually handle task
+        switch (message.getMessageID()) {
+            case INTERESTED_ID:
+                break;
+            case NOT_INTERESTED_ID:
+                break;
+            case HAVE_ID:
+                break;
+            case REQUEST_ID:
+                break;
+            case PIECE_ID:
+                Download.receivePiece(connection, datafile, message.getPiece());
+                // TODO: broadcast HAVE to all peers
+                break;
+            case BITFIELD_ID:
+                break;
+            case HANDSHAKE_ID:
+                break;
+            case CHOKE_ID:
+                Download.receiveChoke(connection);
+                break;
+            case UNCHOKE_ID:
+                Download.receiveUnchoke(connection);
+                break;
+        }
     }
 }
